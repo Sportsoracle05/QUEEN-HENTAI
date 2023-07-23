41c7b8ed79d8d3e605b3b7b630c747501c84c938
Sportsoracle05 <119137579+Sportsoracle05@users.noreply.github.com>
2023-07-23 13:43:33 +0100

    Update QUEEN-HENTAI-donate.js

--- plugins/QUEEN-HENTAI-donate.js
+++ plugins/QUEEN-HENTAI-donate.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 🇱🇰 *DONATE* 🇱🇰
-If you get more info of 💝 Queen Hentai 💝 Please Contact 94740804536 💰`
+If you get more info of 💝 Queen Oracle 💝 Please Contact 2349085771938 💰`
 let img = 'https://telegra.ph/file/7d84f663bc6e7fdac5c66.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
